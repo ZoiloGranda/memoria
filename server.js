9d71d36a6b4a9a10aeeeda154902c8220b83ae7a
Zoilo Granda <zgranda@gmail.com>
2020-04-17 18:22:51 -0400

    Added express server

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const port = process.env.PORT || 8080
 
 app.use(express.static(__dirname+'/dist/memoria'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/memoria/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(port, ()=>{
+ console.log(`listening on ${port}`)
+});
